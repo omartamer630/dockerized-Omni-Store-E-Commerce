5990bd096e52b7b14d24c762a42c0425059b644a
omartamer630 <omartamerdev@gmail.com>
2024-09-11 20:39:54 +0300

    Dockerized app

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,7 @@
 import app from "./app";
 import { connectDb } from "./src/config/database";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 async function startServer() {
   try {
